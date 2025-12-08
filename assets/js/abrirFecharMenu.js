a8769a61980fd50b17f13059ce536edddff4797d
Ruan D elia <ruandelia2@gmail.com>
2025-12-08 14:57:02 -0300

    teste de animação do menu-tablet

--- assets/js/abrirFecharMenu.js
+++ assets/js/abrirFecharMenu.js
@@ -5,18 +5,24 @@ const btnFechar = document.querySelector(".btnFechar")
 /* console.log("Menu Aberto:" + menuAberto + "\nbtnAbrir: " + btnAbrir + "\nbtnFechar: " + btnFechar); */
 
 btnAbrir.addEventListener("click", (event) => {
-    event.preventDefault()
-    menuAberto.classList.remove("displayNone"); // Abre o modal
-    btnAbrir.classList.add("displayNone")
-    btnFechar.classList.remove("displayNone")
-})
+    event.preventDefault();
+    // Ação principal: Adicionar a classe que dispara a animação (translateX(0))
+    menuAberto.classList.add("displayBlock"); 
+    
+    // Ações do botão: Esconde o botão de Abrir e mostra o de Fechar
+    btnAbrir.classList.add("displayNone");
+    btnFechar.classList.remove("displayNone");
+});
 
 btnFechar.addEventListener("click", (evento) => {
-    evento.preventDefault()
-    menuAberto.classList.add("displayNone")
-    btnFechar.classList.add("displayNone")
-    btnAbrir.classList.remove("displayNone")
-})
+    evento.preventDefault();
+    // Ação principal: Remover a classe que dispara a animação (ele volta para translateX(100%))
+    menuAberto.classList.remove("displayBlock");
+    
+    // Ações do botão: Esconde o botão de Fechar e mostra o de Abrir
+    btnFechar.classList.add("displayNone");
+    btnAbrir.classList.remove("displayNone");
+});
 
 document.addEventListener("click", (event) => {
     // 1. O clique não pode ser nos botões de abrir/fechar,
@@ -33,6 +39,7 @@ document.addEventListener("click", (event) => {
     if(estaAberto && cliqueFora && !isBotao){
         // Ações para fechar o menu:
         menuAberto.classList.add("displayNone");
+        menuAberto.classList.remove("displayBlock");
         btnFechar.classList.add("displayNone"); 
         btnAbrir.classList.remove("displayNone");
         
